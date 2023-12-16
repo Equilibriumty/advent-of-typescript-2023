2a3aed22b3942e2c7d9487b4906aad415be83d1e
Equilibriumty <fanflackplay@gmail.com>
2023-12-16 12:06:34 +0200

    16.ts

--- 16.ts
+++ 16.ts
@@ -1,12 +1,12 @@
 // based on https://github.com/TkDodo/aot-2023/blob/main/16.ts
 
 type FindSantaRow<
-  TForest extends Array<any>,
-  Acc extends Array<any> = []
+  TForest extends any[],
+  Counter extends any[] = []
 > = TForest extends [infer Head, ...infer Tail]
   ? Head extends "🎅🏼"
-    ? Acc["length"]
-    : FindSantaRow<Tail, [...Acc, Head]>
+    ? Counter["length"]
+    : FindSantaRow<Tail, [...Counter, Head]>
   : never;
 
 type CastToNumber<T extends string> = T extends `${infer Digit extends number}`
